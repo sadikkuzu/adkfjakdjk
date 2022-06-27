3bb007170ad7f6259d21f92d1c96e9b86f6c605e
SADIK KUZU <sadikkuzu@hotmail.com>
2022-06-27 20:28:50 +0300

    Add command-line flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Parser};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Print crates.io link
+    #[clap(short, long, action = clap::ArgAction::Count)]
+    crates: u8,
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+    let cli = Cli::parse();
+
+    if cli.crates > 0 {
+        println!("https://crates.io/crates/adkfjakdjk")
+    }
 }
